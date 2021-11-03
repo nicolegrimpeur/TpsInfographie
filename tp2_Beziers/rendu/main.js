10bda1fc27d167c7ad61cbf9a1650ed198c646ef
Califano9577 <amaury.giot@student.yncrea.fr>
2021-11-03 17:05:12 +0100

    ajout de ma partie dans le main
    ajout de ma partie + suppression des console log de Quentin

--- tp2_Beziers/rendu/main.js
+++ tp2_Beziers/rendu/main.js
@@ -28,11 +28,10 @@ function main(form) {
     });
 
     // on ajoute tous les points
-    const pointsControle = addPointsControle();
-    const pointsBezier = addPointsBezier(pointsControle);
+    const pointsBezier = addPointsBezier(tableauPoint);
 
     // créé un buffer de points à partir du tableau de points
-    const geometryControle = new THREE.BufferGeometry().setFromPoints(pointsControle);
+    const geometryControle = new THREE.BufferGeometry().setFromPoints(tableauPoint);
     const geometryBezier = new THREE.BufferGeometry().setFromPoints(pointsBezier);
 
     // enregistre tous les points
@@ -46,26 +45,11 @@ function main(form) {
     scene.add(formeBezier);
     if(document.getElementById("zoom").checked == true)
     {
-        autoZoom(pointsControle);
+        autoZoom(tableauPoint);
     }
     renderer.render(scene, camera);
 }
 
-// retourne un tableau de points de controle
-function addPointsControle() {
-    const points = [];
-
-    let el, elSuivant;
-    for (let i = 0; i < form.length; i += 2) {
-        el = form[i];
-        elSuivant = form[i + 1];
-        if (el.name !== 'dezoom' && el.name !== 'bonus')
-            if (el.value !== '' && elSuivant.value !== '')
-                points.push(new THREE.Vector3(parseInt(el.value), parseInt(elSuivant.value), 0));
-    }
-
-    return points;
-}
 
 // retourne le tableau avec les points de la courbe de Bézier
 function addPointsBezier(pointsControle) {
@@ -102,15 +86,10 @@ function autoZoom(tabPoint){
             Ymax = tabPoint[i].y;
         }
     }
-    console.log(Xmin);
-    console.log(Xmax);
-    console.log(Ymin);
-    console.log(Ymax);
     let Xmoy=(Xmax-Xmin)/2;
     let Ymoy=(Ymax-Ymin)/2;
     if(Xmoy>Ymoy){
         let dezoom = Xmax-Xmin;
-        console.log(dezoom);
         camera.position.set(Xmin+Xmoy, Ymin+Ymoy, dezoom);
         camera.lookAt(Xmin+Xmoy,Ymin+Ymoy , dezoom);
 
@@ -131,6 +110,34 @@ function binomial(n, k) {
     for (let x = n - k + 1; x <= n; x++) coeff *= x;
     for (x = 1; x <= k; x++) coeff /= x;
     return coeff;
+}
+
+let tableauPoint = [];
+
+function ajout(form) {
+    if (form.pointFigure.value === "new") {
+        tableauPoint.push(new THREE.Vector3(parseInt(form.xPointAjout.value), parseInt(form.yPointAjout.value), 0))
+
+
+        let div = document.getElementsByName('pointFigure');
+        let option = document.createElement('option');
+        div[0].appendChild(option);
+        option.setAttribute('value', tableauPoint.length);
+        option.textContent = 'Point ' + tableauPoint.length;
+
+        form.xPointAjout.value = '';
+        form.yPointAjout.value = '';
+    } else {
+        tableauPoint[form.pointFigure.value - 1] = new THREE.Vector3(parseInt(form.xPointAjout.value), parseInt(form.yPointAjout.value), 0)
+    }
+    main(form);
+}
+
+function afficherPoint(form){
+    if (form.pointFigure.value !== "new"){
+        form.xPointAjout.value = tableauPoint[form.pointFigure.value - 1].x;
+        form.yPointAjout.value = tableauPoint[form.pointFigure.value - 1].y;
+    }
 }
 
 // permet d'afficher les courbes de Béziers en préselection dans le bonus
